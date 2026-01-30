03a654ec50aff4672f588bde7e5b56f8a3979a66
Nick-1324 <p.nikhilram569@gmail.com>
2026-01-29 20:58:45 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -16,7 +16,7 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 if not GROQ_API_KEY:
     raise ValueError("GROQ_API_KEY not found in environment variables")
 
-client = Groq(api_key=GROQ_API_KEY)
+client = Groq()
 DATA_PATH = "feature_engineered_us_cities.csv"
 
 # --- DATA & MODEL ENGINE ---
